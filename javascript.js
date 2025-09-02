e8bdeec16e9133ea2494a94465d1e876af9db135
Veiko Lemberg <iveiks@gmail.com>
2025-09-03 00:06:26 +0300

    Created basic grid, added basic hover color.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,8 @@
+const mainContainer = document.querySelector('#main-container');
+const GRIDSIZE = 16;
 
+for (i = 0; i < GRIDSIZE; i++) {
+  const gridSquare = document.createElement('div');
+  gridSquare.id = 'grid-square';
+  mainContainer.appendChild(gridSquare);
+}
