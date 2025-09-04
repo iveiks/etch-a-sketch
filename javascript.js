1bdf102e4d690f3ffe2ae7f3c1c5a9b4935e6d00
Veiko Lemberg <iveiks@gmail.com>
2025-09-04 08:59:00 +0300

    Added square color change into JavaScript logic

--- javascript.js
+++ javascript.js
@@ -1,8 +1,37 @@
 const mainContainer = document.querySelector('#main-container');
 const GRIDSIZE = 16;
 
-for (i = 0; i < GRIDSIZE; i++) {
+// Make a square
+function makeSquare() {
   const gridSquare = document.createElement('div');
   gridSquare.id = 'grid-square';
-  mainContainer.appendChild(gridSquare);
+  return gridSquare;
 }
+
+// Make the grid
+function makeGrid(size) {
+  for (i = 0; i < size; i++) {
+    const gridSquare = makeSquare();
+    mainContainer.appendChild(gridSquare);
+    console.log('Added a square');
+  }
+}
+
+makeGrid(GRIDSIZE);
+
+const allSquares = document.querySelectorAll('#grid-square');
+console.log(allSquares.length);
+
+// Change square color when hovering over it
+allSquares.forEach((gridSquare) =>
+  gridSquare.addEventListener('mouseenter', () => {
+    gridSquare.style.backgroundColor = 'blue';
+  })
+);
+
+// Change square color when hovering off of it
+allSquares.forEach((gridSquare) =>
+  gridSquare.addEventListener('mouseleave', () => {
+    gridSquare.style.backgroundColor = 'transparent';
+  })
+);
